9598a994e30fabd261c1c1547b04f8c58a5c1e1a
AllenJohnn <allenjohnjoy2004@gmail.com>
2026-07-10 12:22:00 +0530

    first commit

---
+++ src/components/DeleteCar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function DeleteCar() {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row">
+              <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <label htmlFor="" className="form-input">Car ID</label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <button className="btn btn-success">Delete</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DeleteCar;
---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function ViewAll() {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row">
+              <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+  
+                  
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ViewAll;
